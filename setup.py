492f974f9956d32c3f69bd3952f0fd1061019b7f
Gem Newman <spurll@gmail.com>
2016-04-22 08:49:44 -0500

    Minor architecture refactor. Groundwork for PyPI.

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+
+from setuptools import setup
+
+
+setup(
+    name='tread',
+    packages=['tread'],
+    version='0.1',
+    description='A terminal feed reader.',
+    url='https://github.com/spurll/tread',
+    download_url='https://github.com/spurll/tread/tarball/0.1',
+    author='Gem Newman',
+    author_email='spurll@gmail.com',
+    license='CC BY-SA 4.0',
+    install_requires=[
+        'requests', 'beautifulsoup4', 'pyyaml', 'imgii', 'python-dateutil'
+    ],
+    entry_points = {
+        'console_scripts': ('tread = tread.controller:console_main')
+    },
+    keywords=['rss', 'feed', 'reader'],
+    classifiers=[
+        'Intended Audience :: End Users/Desktop',
+        'Environment :: Console :: Curses',
+        'Development Status :: 4 - Beta',
+        'Topic :: Internet',
+        'Programming Language :: Python :: 3.5'
+    ],
+)
