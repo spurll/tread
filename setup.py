4d6c1e0406b6bb42a91bfa80a493947653fee10c
Gem Newman <spurll@gmail.com>
2022-06-07 20:47:16 -0500

    Update package dependency version requirements

--- setup.py
+++ setup.py
@@ -17,8 +17,13 @@ setup(
     author_email='spurll@gmail.com',
     license='CC BY-SA 4.0',
     install_requires=[
-        'requests', 'beautifulsoup4', 'pyyaml', 'imgii', 'python-dateutil',
-        'sqlalchemy', 'html2text'
+        'requests >= 2.25',
+        'beautifulsoup4 >= 4.11',
+        'pyyaml >= 6',
+        'imgii >= 0.3.1',
+        'python-dateutil >= 2.8',
+        'sqlalchemy >= 1.4',
+        'html2text >= 2020'
     ],
     entry_points={'console_scripts': ('tread = main:console_main')},
     keywords=['rss', 'feed', 'reader'],
@@ -27,7 +32,7 @@ setup(
         'Environment :: Console :: Curses',
         'Development Status :: 4 - Beta',
         'Topic :: Internet',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.9'
     ],
     zip_safe=False,
 )
